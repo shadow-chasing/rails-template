f38d143a1a8e78f16bb24f484de1f6b27864f022
shadow-chasing <playswithsharkmedia@gmail.com>
2017-09-21 20:36:39 +0100

    added helper for title and active link, message fade js

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
       link_to link_text, link_path
     end
   end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "my blog"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
